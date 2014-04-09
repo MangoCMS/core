51d3c857530557d57a248c08be41c554610e80f2
Eric Hocking <erich@xaos-ia.com>
2014-04-09 13:17:18 -0400

    Update init.php

--- inc/init.php
+++ inc/init.php
@@ -1,7 +1,31 @@
 <?php
 
+if (!defined('IN_MANGOCORE'))
+{
+  //do not allow access to file
+  die("Direct initialization of this file is not allowed.<br /><br />Please make sure IN_MANGOCORE is defined.");
+}
+
+/* Defines the root directory for MangoCMS.
+
+	Uncomment the below line and set the path manually
+	if you experience problems.
+
+	Always add a trailing slash to the end of the path.
+
+	* Path to your copy of MangoCMS
+ */
+//define('MANGOCORE_ROOT', "./");
+
+// Attempt autodetection
+if(!defined('MANGOCORE_ROOT'))
+{
+  define('MANGOCORE_ROOT', dirname(dirname(__FILE__))."/");
+}
+
+
 //load the Symfony classes
-require_once __DIR__.'/../vendor/symfony/class-loader/Symfony/Component/ClassLoader/UniversalClassLoader.php';
+require_once MANGOCORE_ROOT.'vendor/symfony/class-loader/Symfony/Component/ClassLoader/UniversalClassLoader.php';
  
 use Symfony\Component\ClassLoader\UniversalClassLoader;
  
