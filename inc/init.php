17b67112b311139583aa6324dd0085fefeca9381
Eric Hocking <erich@xaos-ia.com>
2014-04-09 13:13:09 -0400

    Create init.php

---
+++ inc/init.php
@@ -0,0 +1,10 @@
+<?php
+
+//load the Symfony classes
+require_once __DIR__.'/../vendor/symfony/class-loader/Symfony/Component/ClassLoader/UniversalClassLoader.php';
+ 
+use Symfony\Component\ClassLoader\UniversalClassLoader;
+ 
+$loader = new UniversalClassLoader();
+$loader->register();
+ 
